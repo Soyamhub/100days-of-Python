e9ce8a88d1850f93977b39da79d3fce4caf2c066
Soyamhub <soyamshubhamswain@gmail.com>
2025-02-08 00:47:12 +0530

    Adding a program where we have to add the inputed two digit number and print it without using loops.

---
+++ Day 2/TwodigitAdd.py
@@ -0,0 +1,3 @@
+#Write a program that adds the digits in a 2 digit number,e.g if the input was 35, then the output should be 8.
+two_digit_number = input()
+print(int(two_digit_number[0]) + int(two_digit_number[1]))
