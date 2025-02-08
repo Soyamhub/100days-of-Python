cc1e186e51012a39667ad3993412489e6b37d49c
Soyamhub <soyamshubhamswain@gmail.com>
2025-02-08 14:59:49 +0530

    Adding a BMI calculator program

---
+++ Day 2/BMICalculator.py
@@ -0,0 +1,4 @@
+weight = float(input("Give the weight of a person (in kg)\n"))
+height = float(input("Give the height of a person (in meter)\n"))
+BMI = weight / (weight * weight)
+print(f"The BMI index of this person is {BMI}")
