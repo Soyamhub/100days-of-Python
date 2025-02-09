b6d3ea1e44562665df43590234b2a39353f41d67
Soyamhub <soyamshubhamswain@gmail.com>
2025-02-09 23:41:56 +0530

    Adding a code where we calculate the reamining weeks left in yours life

---
+++ Day 2/WeekCalculator.py
@@ -0,0 +1,5 @@
+#This program helps to find your remaining week left
+age = int(input("Enter your age\n"))
+total_age = 90
+remaining_age = 90 - age
+print(f"You have {remaining_age*52} weeks left")
