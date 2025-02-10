836cb1e8f94ec6bd0599c605572a205ecd272dd9
Soyamhub <soyamshubhamswain@gmail.com>
2025-02-10 22:55:51 +0530

    Make some changes in Treasure_map code

--- Day 4/Treasure_map.py
+++ Day 4/Treasure_map.py
@@ -5,11 +5,8 @@ map = [line1, line2, line3]
 print("Hiding your treasure ? 'X' mark to the place.")
 position = input("Give the position like rows are in letter from A and columns are in numbers from 1(B2,C1....)\n")
 letter = position[0].lower()
-row = ['a', 'b', 'c']
-if letter == row[0]:
-    map[0][int(position[1])] = 'X'
-elif letter == row[1]:
-    map[1][int(position[1])] = 'X'    
-else:
-    map[2][int(position[1])] = 'X'
+abc = ['a', 'b', 'c']
+row_index = abc.index(letter)
+column_index = int(position[1]) - 1
+map[row_index][column_index] = 'X'
 print(f"{line1}\n{line2}\n{line3}")    
